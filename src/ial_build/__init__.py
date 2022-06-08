21f7c961faa0cb02594f1e840c097701a9d2d86a
Alexandre Mary <alexandre.mary@meteo.fr>
2022-06-08 11:57:23 +0200

    VERSION taken out __init__

--- src/ial_build/__init__.py
+++ src/ial_build/__init__.py
@@ -7,10 +7,11 @@
 from __future__ import print_function, absolute_import, unicode_literals, division
 
 import os
+import io
 
 """
 IAL (IFS-ARPEGE & LAM:ALADIN-AROME-ALARO-HARMONIE) source code management.
 """
 
-__version__ = '1.1.3'
 package_rootdir = os.path.dirname(os.path.dirname(os.path.realpath(__path__[0])))  # realpath to resolve symlinks
+__version__ = io.open(os.path.join(package_rootdir, 'VERSION'), 'r').read().strip()
